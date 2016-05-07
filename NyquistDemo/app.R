e1947d2174d97c8f5e4cae1e46c781cfa52cb2b5
exixx <exixx@exixx.com>
2016-05-07 18:35:07 -0400

    Add the single page Shiny app to the repo.
    Has only one simple control so far. See README for details.

---
+++ NyquistDemo/app.R
@@ -0,0 +1,100 @@
+
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- shinyUI(fluidPage(
+   
+   # Application title
+   titlePanel("Nyquist Demonstration"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("freq",
+                     "Frequency Parameter:",
+                     min = 75,
+                     max = 250,
+                     value = 80)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+))
+
+# Define server logic required to draw a histogram
+server <- shinyServer(function(input, output) {
+   
+   output$distPlot <- renderPlot({
+     make_config <- function (rows, cols) { #}, row_names, col_names) {
+       a = array(0, 
+                 dim = c(rows, cols),
+                 #dimnames = list(c(row_names), c(col_names))
+       )
+       b = data.frame(a)
+       return(b)
+     }
+     
+     num_samp <- 5000
+     num_freq <- 1024
+     
+     a_frame <- make_config(num_samp, num_freq)
+     
+     az_deg <- seq(0, 359.5, length.out = num_samp)
+     az_rad <- seq(0, 2 * pi, length.out = num_samp)
+     
+     drive_func <- function (amp, omega, ttt) {
+       a_line <-  amp *cos((omega* pi/input$freq)* ttt )
+     }
+     
+     amps <- 1:num_freq
+     
+     for( ii in amps ) {
+       a_frame[ii] <- drive_func(ii,5, az_deg)
+       
+     }
+     jj <- 1:num_samp
+     a_frame[jj,] = a_frame[jj,] *jj/200
+     
+     colrs <- rainbow(num_freq)
+     
+     file_name = "Test Sines"
+     plotaline <- function(xx) {plot(1:num_samp,a_frame[,xx], 
+                                     type = "l",
+                                     col = colrs[xx],
+                                     ylim = range(a_frame[,xx]),#*(3/5),
+                                     ylab = "Some Bullshit Magnitude (dB Bullshit)",
+                                     xlab = "Azimuth (degrees or something))",
+                                     main = paste("Test Plot", file_name),
+                                     bg = "white",
+                                     axes = FALSE
+     )}
+     
+     add_a_line <- function(xx) {lines(1:num_samp,a_frame[,xx], 
+                                       type = "l",
+                                       col = colrs[xx],
+                                       #ylim = range(a_frame[,xx]),
+                                       ylab = "Some Bullshit Magnitude (dB Bullshit)",
+                                       xlab = "Azimuth (degrees or something))",
+                                       main = paste("Test Plot", file_name),
+                                       bg = "white"
+     )}
+     
+     divisor <- 300
+     
+     
+     
+     plotaline(num_freq)
+     
+     for (i in seq(num_freq, 1, by = -100)) {
+       add_a_line(i)
+     }
+   })
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- README.md
+++ README.md
@@ -4,3 +4,17 @@ This program is to be a Nyquist demonstration plotter in R/Shiny
 It will plot a set of increasing amplitude sine waves to demonstrate the Nyquist sampling criteria graphically.
 
 The plotter will plot several pi worth of cycles and then with a slider allow the user to modify the sample rate of the signal to demonstrate what the minimum criteria should be to sample a given frequency signal. I plan to add to the demonstration a frequency modulated signal and some other demonstration bells and whistles.
+
+This commit:
+
+  Adds the single page Shiny app with one slider that controls the number of cycles of the driver function.
+
+
+To Do:
+
+ The slider needs to be modified to show the number of cycles rather than the divisor for the frequency in the driver.
+  Needs a divisor for number of plotted lines as well.
+  Then add a sampler.
+  
+
+
