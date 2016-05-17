ef4dc7cf68866a4e8a1983b43667bc5c51753f50
exixx <exixx@exixx.com>
2016-05-17 19:57:05 -0400

    Controls to the bottom, sampling slider, and shinythemes edit.

--- NyquistDemo/app.R
+++ NyquistDemo/app.R
@@ -1,26 +1,40 @@
-
+# nyquist demonstration Shiny app
+# written by exixx
+# exixx @ exixx . com
 
 library(shiny)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
+
+
+
+# Define UI for application - give me a plot and a slider for the cycles to start
 ui <- shinyUI(fluidPage(
+  theme = shinytheme("flatly"),
    
    # Application title
    titlePanel("Nyquist Demonstration"),
    
-   # Sidebar with a slider input for number of bins 
+  plotOutput("sinePlot"),
+   # Sidebar with a slider input for number of cycles. Irritatingly higher is fewer cycles.
    sidebarLayout(
       sidebarPanel(
          sliderInput("freq",
-                     "Frequency Parameter:",
-                     min = 75,
-                     max = 250,
-                     value = 80)
+                     "1/f (higher = less cycles):",
+                     min = 35,
+                     max = 2400,
+                     value = 80),
+         sliderInput("samps",
+                     "Number of samples:",
+                     min = 8,
+                     max = 5000,
+                     value = 100)
+         
       ),
       
-      # Show a plot of the generated distribution
+      # Show a plot of the generated set of sine waves
       mainPanel(
-         plotOutput("distPlot")
+         
       )
    )
 ))
@@ -28,41 +42,40 @@ ui <- shinyUI(fluidPage(
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
    
-   output$distPlot <- renderPlot({
-     make_config <- function (rows, cols) { #}, row_names, col_names) {
-       a = array(0, 
-                 dim = c(rows, cols),
-                 #dimnames = list(c(row_names), c(col_names))
-       )
-       b = data.frame(a)
-       return(b)
-     }
+   output$sinePlot <- renderPlot({
      
-     num_samp <- 5000
-     num_freq <- 1024
+     #num_samp <- 50 #original 5000
+     num_freq <- 2048 #500 #original 1024
+     divisor <- 100
+     row_names <- paste("t", (1:input$samps), sep = "")
+     col_names <- paste("f", (1:num_freq), sep = "")
+     a = array(0, dim = c(input$samps, num_freq), dimnames = list(c(row_names), c(col_names)))
      
-     a_frame <- make_config(num_samp, num_freq)
+     a_frame <- data.frame(a)
      
-     az_deg <- seq(0, 359.5, length.out = num_samp)
-     az_rad <- seq(0, 2 * pi, length.out = num_samp)
-     
+     az_deg <- seq(0, 719.5, length.out = input$samps)
+     az_rad <- seq(0, 2 * pi, length.out = input$samps)
      drive_func <- function (amp, omega, ttt) {
        a_line <-  amp *cos((omega* pi/input$freq)* ttt )
      }
      
      amps <- 1:num_freq
      
+     # this loop sucks. time to learn lapply
      for( ii in amps ) {
        a_frame[ii] <- drive_func(ii,5, az_deg)
-       
+
      }
-     jj <- 1:num_samp
-     a_frame[jj,] = a_frame[jj,] *jj/200
+     # unlist(lapply(amps, function(ii) (a_frame[ii] <- drive_func(ii, 5, az_deg))))
+     jj <- 1:input$samps
+     a_frame[jj,] = a_frame[jj,] *jj/divisor
      
+     #set up a list of color numbers for the lines to make the display aesthetically pleasing.
      colrs <- rainbow(num_freq)
      
-     file_name = "Test Sines"
-     plotaline <- function(xx) {plot(1:num_samp,a_frame[,xx], 
+     file_name = "Test Sines" #This can be modified to generate a file name/title.
+     #I probably won't do that but I'm leaving it in anyway.
+     plot_a_line <- function(xx) {plot(1:input$samps,a_frame[,xx], 
                                      type = "l",
                                      col = colrs[xx],
                                      ylim = range(a_frame[,xx]),#*(3/5),
@@ -73,7 +86,7 @@ server <- shinyServer(function(input, output) {
                                      axes = FALSE
      )}
      
-     add_a_line <- function(xx) {lines(1:num_samp,a_frame[,xx], 
+     add_a_line <- function(xx) {lines(1:input$samps,a_frame[,xx], 
                                        type = "l",
                                        col = colrs[xx],
                                        #ylim = range(a_frame[,xx]),
@@ -83,11 +96,11 @@ server <- shinyServer(function(input, output) {
                                        bg = "white"
      )}
      
-     divisor <- 300
      
      
      
-     plotaline(num_freq)
+     
+     plot_a_line(num_freq)
      
      for (i in seq(num_freq, 1, by = -100)) {
        add_a_line(i)
--- README.md
+++ README.md
@@ -3,18 +3,28 @@ This program is to be a Nyquist demonstration plotter in R/Shiny
 
 It will plot a set of increasing amplitude sine waves to demonstrate the Nyquist sampling criteria graphically.
 
-The plotter will plot several pi worth of cycles and then with a slider allow the user to modify the sample rate of the signal to demonstrate what the minimum criteria should be to sample a given frequency signal. I plan to add to the demonstration a frequency modulated signal and some other demonstration bells and whistles.
+The plotter displays a plot of varying numbers of cycles and sample rate via sliders to calculate a data frame full of amplitude varying sine waves of varying sample rate. This set was generated to provide an aesthetically pleasing display for manipulation to demonstrate the requirements for Nyquist sampling rates. This set consists of sine waves of varying amplitudes at a constant frequency, sampled at a settable rate.
+
+The set is calculated each time the sliders are manipulated, which can result in a slight delay. The upper sample rate is far more than is required to adequately sample the signals, but I also wanted to get an idea of how R behaves when carrying around 10M or so of a data frame. Quite well, it seems, though the delay is a tad bit more than I would like.
 
 This commit:
+  - Moved the controls to the bottom to make the plot larger.
+  - Adjusted the frequency slider to cover the desired range of frequencies, but irritatingly it is reversed from what I want. I'll have to fiddle with the equation to get it right.
+  - Having trouble figuring out exactly how many cycles I want to display and getting a solid explanation of the signals formulated.
+  - Added a slider for number of samples, and to get the range for the nyquist display correct. Low sample rates, harmonic sampling error, and various under/oversampling situations can be observed.
+  - Added shinythemes and added a theme, but really want to use the yeti bootstrap theme, so I'll be establishing a www/ directory next commit. Also want to include a graphic, so I'll need it.
+  
 
-  Adds the single page Shiny app with one slider that controls the number of cycles of the driver function.
+Old Commits:
+  - Adds the single page Shiny app with one slider that controls the number of cycles of the driver function.
 
 
 To Do:
 
- The slider needs to be modified to show the number of cycles rather than the divisor for the frequency in the driver.
+ The slider STILL needs to be modified to show the number of cycles rather than the divisor for the frequency in the driver.
   Needs a divisor for number of plotted lines as well.
-  Then add a sampler.
+  Perhaps a thickness setting in conjunction with the divisor more nicer looking displays?
+  
   
 
 
